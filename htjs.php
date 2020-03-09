05acb668c76fe1544dd5b75fad71845f029d562b
Daniel2019Creator1 <57808482+Daniel2019Creator1@users.noreply.github.com>
2020-03-08 21:25:36 -0400

    Add files via upload

---
+++ htjs.php
@@ -0,0 +1,23 @@
+    // <script>
+
+        function ptv1() {
+        var pixel = document.createElement('img');
+        var referrer = encodeURIComponent(document.referrer);
+        var src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'hubt.pornhub.com/pix.php?s=3&h=eecf49f1f7044157a8c1926a51b450d3&t=1583716112.9496&r=https%3A%2F%2Fwww.pornhub.com%2Fview_video.php%3Fviewkey%3Dph5d1963f999d0d&q=hqsbgg326jq8ze9whngo43etusbx6r1o&p=1';
+        pixel.setAttribute('id', "affpx101");
+        pixel.src = src;
+    }
+
+    setTimeout(function() {
+        var f = 0;
+        if(top!=self) {
+            f = 1;
+        }
+        var pixel = document.createElement('img');
+        var referrer = encodeURIComponent(document.referrer);
+        var src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'hubt.pornhub.com/pix.php?s=3&h=0cfb3d999f7648d00a894eabc6fb6a9d&t=1583716112.9496&r=https%3A%2F%2Fwww.pornhub.com%2Fview_video.php%3Fviewkey%3Dph5d1963f999d0d&q=hqsbgg326jq8ze9whngo43etusbx6r1o&p=0&f=' +f ;
+        pixel.setAttribute('id', "affpx101");
+        pixel.src = src;
+    }, 1);
+    // </script>
+    
